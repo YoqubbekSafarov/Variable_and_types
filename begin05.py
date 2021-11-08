756069cf0222b6763afd451aeb6cd48ea6049db6
Safarov_Yoqubbek <yoqubbeksafarov1997@gmail.com>
2021-11-08 22:22:38 +0300

    begin05

---
+++ begin05.py
@@ -0,0 +1,10 @@
+# a o'zgaruvchiga 7 ni taminlang;
+# b o'zgaruvchiga 3 ni taminlang;
+# c o'zgaruvchiga 5 ni taminlang;
+# a, b, c o'zgaruvchilar yig'indisini chop eting;
+
+a =7 
+b = 3
+c =5
+x=a+b+c
+print(x)
